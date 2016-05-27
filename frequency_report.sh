800bb5fcb239c1eb45a1282639b817a703156d54
Divye Kapoor <divyekapoor@gmail.com>
2016-05-26 23:42:03 -0700

    Comment everything.

--- frequency_report.sh
+++ frequency_report.sh
@@ -23,32 +23,66 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Stop executing on error. Optional if you're running this line by line.
 set -e
 
-iconv -c -f utf-8 -t ascii JaneEyre.txt | tr 'A-Z' 'a-z' | tr -s "\n\r." " " |  tr -d "#%\$^@*_;?:.,\'\"/-\!()&[]{}" | tr -s ' ' '\n' > JaneEyre.normalized.txt
-iconv -c -f utf-8 -t ascii WutheringHeights.txt | tr 'A-Z' 'a-z' | tr -s "\n\r." " " |  tr -d "#%\$^@*_;?:.,\'\"/-\!()&[]{}" | tr -s ' ' '\n' > WutheringHeights.normalized.txt
-cat JaneEyre.normalized.txt WutheringHeights.normalized.txt > CombinedWorks.normalized.txt
+# Normalize the input texts:
+# Step 1: Convert to ascii from utf-8.
+# Step 2: Lowercase all the characters.
+# Step 3: Replace all periods and newlines with a space, squeezing adjacent spaces.
+# Step 4: Delete all the special characters.
+# Step 5: Replace all spaces by newlines, making the text 1 word per line.
+#         Output the contents to a temp file.
+iconv -c -f utf-8 -t ascii JaneEyre.txt | tr 'A-Z' 'a-z' | tr -s "\n\r." " " |  tr -d "#%\$^@*_;?:.,\'\"/-\!()&[]{}" | tr -s ' ' '\n' > JaneEyre.normalized.tmp
+iconv -c -f utf-8 -t ascii WutheringHeights.txt | tr 'A-Z' 'a-z' | tr -s "\n\r." " " |  tr -d "#%\$^@*_;?:.,\'\"/-\!()&[]{}" | tr -s ' ' '\n' > WutheringHeights.normalized.tmp
 
-cat JaneEyre.normalized.txt | sort | uniq -c | awk -F' ' "{ freq = \$1; word = \$2; total_words = $(wc -w < JaneEyre.normalized.txt); relative_freq = freq / total_words; print word \",\" freq \",\" relative_freq; }" | sort  > JaneEyre.report.txt
+# Concatenate the two normalized texts into a single CombinedWork.
+cat JaneEyre.normalized.tmp WutheringHeights.normalized.tmp > CombinedWorks.normalized.tmp
 
-cat WutheringHeights.normalized.txt | sort | uniq -c | awk -F' ' "{ freq = \$1; word = \$2; total_words = $(wc -w < WutheringHeights.normalized.txt); relative_freq = freq / total_words; print word \",\" freq \",\" relative_freq; }" | sort > WutheringHeights.report.txt
+# Gather statistics from each normalized work in the following format:
+# word,word_frequency,relative_frequency
+# Step 1: Sort the wordlist from each text.
+# Step 2: Uniquify the wordlist into |count <space> word| format (one per line).
+# Step 3a: Using wc, calculate the total number of words in the normalized text and insert it into the awk script.
+# Step 3b: awk generates a CSV file in the format: |word,freq,relative_freq|.
+# Step 4: Sort each line alphabetically and save it to a temp file. This generates a temp file sorted by words.
+sort < JaneEyre.normalized.tmp | uniq -c | awk -F' ' "{ freq = \$1; word = \$2; total_words = $(wc -w < JaneEyre.normalized.tmp); relative_freq = freq / total_words; print word \",\" freq \",\" relative_freq; }" | sort  > JaneEyre.report.csv
+sort < WutheringHeights.normalized.tmp | uniq -c | awk -F' ' "{ freq = \$1; word = \$2; total_words = $(wc -w < WutheringHeights.normalized.tmp); relative_freq = freq / total_words; print word \",\" freq \",\" relative_freq; }" | sort > WutheringHeights.report.csv
 
-cat CombinedWorks.normalized.txt | sort | uniq -c | sort -rn | head -1000 | awk -F' ' "{ print \$2 \",\" \$1 }" | sort > CombinedWorks.report.txt
+# Gather statistics for the combined work (top 1000 words):
+# Step 1: Sort the bag of words.
+# Step 2: Uniquify the bag of words into the |count word| format.
+# Step 3: Sort numerically in descending order (by frequency count).
+# Step 4: Pick the first 1000 entries.
+# Step 5: Reformat the entries into |word,frequency| format.
+sort < CombinedWorks.normalized.tmp | uniq -c | sort -rn | head -1000 | awk -F' ' "{ print \$2 \",\" \$1 }" | sort > CombinedWorks.report.csv
 
-join -t, CombinedWorks.report.txt JaneEyre.report.txt  > JoinedFrequencies.partial.txt
-join -t, -v1 CombinedWorks.report.txt JaneEyre.report.txt | sed -e 's/$/,0,0/' >> JoinedFrequencies.partial.txt
-sort < JoinedFrequencies.partial.txt > JoinedFrequencies.partial_sorted.txt
+# Join statistics for the top 1000 words for the words from the combined work and JaneEyre.
+# The first command joins words that are common to the top 1000 of the CombinedWork and JaneEyre.
+# The second command prints words that are present only in the CombinedWork (replacing missing values with 0).
+# The third command sorts the entire joined dataset again by words.
+join -t, CombinedWorks.report.csv JaneEyre.report.csv  > JoinedFrequencies.partial.tmp
+join -t, -v1 CombinedWorks.report.csv JaneEyre.report.csv | sed -e 's/$/,0,0/' >> JoinedFrequencies.partial.tmp
+sort < JoinedFrequencies.partial.tmp > JoinedFrequencies.partial_sorted.tmp
 
-join -t, JoinedFrequencies.partial_sorted.txt WutheringHeights.report.txt > JoinedFrequencies.all.txt
-join -t, -v1 JoinedFrequencies.partial_sorted.txt WutheringHeights.report.txt | sed -e 's/$/,0,0/' >> JoinedFrequencies.all.txt
-sort < JoinedFrequencies.all.txt > JoinedFrequencies.all_sorted.txt
+# Repeat the dataset join (by word) with the WutheringHeights dataset.
+join -t, JoinedFrequencies.partial_sorted.tmp WutheringHeights.report.csv > JoinedFrequencies.all.tmp
+join -t, -v1 JoinedFrequencies.partial_sorted.tmp WutheringHeights.report.csv | sed -e 's/$/,0,0/' >> JoinedFrequencies.all.tmp
+sort < JoinedFrequencies.all.tmp > JoinedFrequencies.all_sorted.tmp
 
-awk -F, '{ print $4 - $6 " " $1 }' < JoinedFrequencies.all_sorted.txt | sort -n > DistinctiveWords.txt
+# Step 1: Take the difference between the relative_frequencies between the JaneEyre dataset and the WutheringHeights
+# for each of the top 1000 words.
+# Step 2: Sort numerically according to the difference. Words with a positive
+# score are more distinctive for JaneEyre. Words with a negative score are more
+# so for WutheringHeights. Save to a temp file.
+awk -F, '{ print $4 - $6 " " $1 }' < JoinedFrequencies.all_sorted.tmp | sort -n > DistinctiveWords.tmp
 
 echo "DistinctiveWords for JaneEyre"
 echo "============================="
-tail -10 DistinctiveWords.txt | awk '{ print $2 }'
+tail -10 DistinctiveWords.tmp | awk '{ print $2 }'
 echo
 echo "Distinctive words for WutheringHeights"
 echo "======================================"
-head -10 DistinctiveWords.txt | awk '{ print $2 }'
+head -10 DistinctiveWords.tmp | awk '{ print $2 }'
+
+rm *.tmp
